37246624f606920e93a21cb8b33ebc14bc332ce2
Muhammad Aziz <mrmaziz@proton.me>
2026-03-11 19:17:34 +0500

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,28 +10,16 @@ const config: Config = {
   title: 'Azeon',
   tagline: 'Exploring complexity. Building solutions.',
   favicon: 'img/favicon.ico',
-
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  url: 'https://mrazizz.github.io',
+  baseUrl: '/azeon/',
   organizationName: 'mrazizz', // Usually your GitHub org/user name.
-  projectName: 'P-vs-NP-Explained', // Usually your repo name.
-
+  projectName: 'azeon', // Usually your repo name.
   onBrokenLinks: 'throw',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
